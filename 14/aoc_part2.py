f7c0703695aa4b8ce8154fc29359c93f323b45aa
James Horne <jamesmhorne90@gmail.com>
2022-12-29 00:47:55 -0500

    Completed Day 14

---
+++ 14/aoc_part2.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+class Cave:
+    def __init__(self, cave_data) -> None:
+        self.x_min, self.x_max, self.y_max = self.find_size(cave_data)
+        self.cave = np.full((self.y_max, self.x_max), ".", dtype=str)
+        self.full = False
+        self.sand_start = (0, 500)
+        self.sand = 0
+
+        self.cave[self.sand_start] = "+"
+
+        for line in cave_data:
+            for x in range(len(line) - 1):
+                self.draw_rock(line[x], line[x + 1])
+        
+        self.cave[self.y_max - 1] = "#"
+    
+    def draw_rock(self, start, end):
+        if start[0] != end[0]:
+            step = 1
+            if start[0] > end[0]:
+                step = -1
+            for x in range(start[0], end[0] + step, step):
+                self.cave[end[1], x] = '#'
+        else:
+            step = 1
+            if start[1] > end[1]:
+                step = -1
+            for y in range(start[1], end[1] + step, step):
+                self.cave[y, end[0]] = '#'
+
+    def find_size(self, cave_data):
+        xs = list()
+        ys = list()
+        for line in cave_data:
+            for point in line:
+                xs.append(point[0])
+                ys.append(point[1])
+        return min(xs), max(xs) + 1, max(ys) + 3
+
+    def drop_sand(self):
+        if self.full:
+            return
+        
+        self.sand += 1
+        falling_sand_pos = [self.sand_start[0], self.sand_start[1]]
+        sand_at_rest = False
+
+        try:
+            while not sand_at_rest:
+                if falling_sand_pos[1] + 1 >= len(self.cave[0]):
+                    self.add_col()
+                if self.cave[falling_sand_pos[0] + 1, falling_sand_pos[1]] not in ["#", "o"]: # check down
+                    falling_sand_pos[0] += 1
+                elif self.cave[falling_sand_pos[0] + 1, falling_sand_pos[1] - 1] not in ["#", "o"]: # check down left
+                    falling_sand_pos[0] += 1
+                    falling_sand_pos[1] -= 1
+                elif self.cave[falling_sand_pos[0] + 1, falling_sand_pos[1] + 1] not in ["#", "o"]: # check down left
+                    falling_sand_pos[0] += 1
+                    falling_sand_pos[1] += 1
+                else:
+                    sand_at_rest = True
+            
+            self.cave[tuple(falling_sand_pos)] = "o"
+
+            if self.cave[self.sand_start] == "o":
+                raise Exception("full")
+        except Exception as e:
+            print(e)
+            self.full = True
+    
+    def add_col(self):
+        col = np.full((self.y_max, 1), ".", dtype=str)
+        col[self.y_max - 1, 0] = '#'
+        self.cave = np.hstack((self.cave, col))
+
+
+with open('input.txt') as f:
+    cave_data = [[[int(z) for z in y.split(',')] for y in x.split(' -> ')] for x in f.read().split('\n')]
+
+cave = Cave(cave_data)
+
+while not cave.full:
+    cave.drop_sand()
+
+for line in cave.cave:
+    print(''.join(line))
+
+print(cave.sand)
